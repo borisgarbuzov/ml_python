06b1659cff951ae2d56a82a7b4efe69fcc200621
statistics101 <boris.garbuzov@gmail.com>
2018-10-17 09:04:08 -0400

    space

--- ch2/experiment.py
+++ ch2/experiment.py
@@ -5,14 +5,25 @@ def matmult(m, v):
         w[row] = reduce(lambda x,y: x+y, map(lambda x,y: x*y, m[row], v))
     return w
 
+ 
+a = [4, 5, 6]
+b = [1,2,3]
+c = [i * j for i, j in zip(a, b)]
+print ("c = ", c)
+print ("zip(a, b) = ", zip(a, b))
+d = [(i, j) for i, j in zip(a, b)]
+print ("d = ", d)
+myCortege = (1,2)
+print ("myCortege = ", myCortege)
+
 
 #................................................
 
-m, n = 2, 3
-vec = range(1, n+1)
-mat = [map(lambda x: i*n+x+1, range(n)) for i in range(m)]
-print ('vec=', vec)
-print ('mat=', mat)
-print ('mat . vec=', matmult(mat, vec))
+# m, n = 2, 3
+# vec = range(1, n+1)
+# mat = [map(lambda x: i*n+x+1, range(n)) for i in range(m)]
+# print ('vec=', vec)
+# print ('mat=', mat)
+# print ('mat . vec=', matmult(mat, vec))
 
 
