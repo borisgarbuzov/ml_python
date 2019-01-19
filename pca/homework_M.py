47520941d4a4fad7f4b8524d376c7bba747207b1
statistics101 <boris.garbuzov@gmail.com>
2019-01-19 09:58:31 -0500

    matrix

--- pca/homework_M.py
+++ pca/homework_M.py
@@ -17,3 +17,27 @@ cov_std = np.cov(X_std, Y_std, bias=True)
 print("cov =", cov[0][1])
 print("cor =", cor[0][1])
 print("cov_std =", cov_std[0][1])
+
+xMatrix = []
+xMatrix.append(X)
+xMatrix.append(Y)
+xMatrix = np.array(xMatrix)
+print("xMatrix =\n", xMatrix)
+
+covMatrixBias = np.cov(xMatrix, bias=True)
+print("covMatrixBias =\n", covMatrixBias)
+covMatrixNoBias = np.cov(xMatrix, bias=False)
+print("covMatrixNoBias =\n", covMatrixNoBias)
+
+corMatrix = np.corrcoef(xMatrix)
+print("corMatrix =\n", corMatrix)
+
+
+
+
+
+
+
+
+
+
