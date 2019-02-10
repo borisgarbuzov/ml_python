6c1877a004603741a236e943968deb8ae3d2aa93
statistics101 <boris.garbuzov@gmail.com>
2019-02-10 00:17:39 -0500

    pca

--- pca/PCA_skl.py
+++ pca/PCA_skl.py
@@ -20,13 +20,56 @@ df_wine.columns = ['Class label', 'Alcohol', 'Malic acid', 'Ash',
 # print ("df_wine.head() =", df_wine.head())
 
 
-
-
 from sklearn.decomposition import PCA
 
 pca = PCA(n_components = 2)
-X_train_pca = pca.fit_transform(X_train_std)
+x_train_pca = pca.fit_transform(x_train_std)
+print ("x_train_pca =", x_train_pca)
+print ("x_train_pca.shape =", x_train_pca.shape)
+print ("x_train_std.shape =", x_train_std.shape)
+x_test_pca = pca.transform(x_test_std)
+print ("x_test_pca =", x_test_pca)
+print ("x_test_pca.shape =", x_test_pca.shape)
 
-# print ("pca.explained_variance_ratio_ =", pca.explained_variance_ratio_)
+# fit regression on both - pca and original
+
+import time
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+timeOriginalStart = time.time()
+logisticClassifierOriginal = LogisticRegression(random_state=0,
+                                                C=1)
+logisticClassifierOriginal.fit(X = x_train_std,
+                               y = y_train)
+print("performace quality of logisticClassifierOriginal",
+      accuracy_score(y_test, logisticClassifierOriginal.predict(x_test_std)))
+timeOriginalEnd = time.time()
+originalDuration = timeOriginalEnd - timeOriginalStart
+print("duration of the original classifier work", originalDuration)
+
+timePcaStart = time.time()
+logisticClassifierPca = LogisticRegression(random_state=0,
+                                                C=1)
+logisticClassifierPca.fit(X = x_train_pca,
+                               y = y_train)
+print("performace quality of logisticClassifierPca",
+      accuracy_score(y_test, logisticClassifierPca.predict(x_test_pca)))
+timePcaEnd = time.time()
+pcaDuration = timePcaEnd - timePcaStart
+print("duration of the PCA classifier work", pcaDuration)
+durationDifference = originalDuration - pcaDuration
+print("durationDifference", durationDifference)
+print("durationDifference >= 0", durationDifference >= 0)
+
+
+
+
+
+
+
+
+
+
 
 
