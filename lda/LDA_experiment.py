80d085f4ae83095189015f8d338b079cef56effb
statistics101 <boris.garbuzov@gmail.com>
2019-02-14 10:02:23 -0500

    lda

--- lda/LDA_experiment.py
+++ lda/LDA_experiment.py
@@ -45,6 +45,40 @@ print ("xc1 =\n", xc1)
 xc2 = x [y==2]
 print ("xc2 =\n", xc2)
 
+meanClass1 = np.mean(xc1, axis=0)
+print ("meanClass1 =", meanClass1)
+meanClass2 = np.mean(xc2, axis=0)
+print ("meanClass2 =", meanClass2)
+xc1Centered = xc1 - meanClass1
+print ("xc1Centered =\n", xc1Centered)
+xc2Centered = xc2 - meanClass2
+print ("xc2Centered =\n", xc2Centered)
+
+# outer product of each row of class 1
+# add them
+# cycle over the rows of the first class
+# lines 1,2 or in Python 0, 1
+sSize = xc1Centered.shape[0]
+print ("rows in  xc1Centered:", sSize)
+s1Matrix = np.zeros_like(xc1Centered)
+print ("s1Matrix =\n", s1Matrix)
+
+for rowIndex in range(0, 2) :
+    currentRow = xc1Centered [rowIndex, :]
+    print("currentRow =", currentRow)
+    currentOuterProduct = np.outer(currentRow, currentRow)
+    print("currentOuterProduct =\n", currentOuterProduct)
+    s1Matrix += currentOuterProduct
+print ("s1Matrix =\n", s1Matrix)
+
+# same for the second class
+
+
+
+
+
+
+
 
 
 
