f772ae7af095518b1095415765a9110c446eb2d6
Maxim Makovskiy <makovskiyms@gmail.com>
2019-04-03 18:00:58 +0600

    SVD matrix multiplication

--- SVD/SVDExample.py
+++ SVD/SVDExample.py
@@ -1,1 +1,26 @@
-abc
+import numpy as np
+
+
+def matrix_multiplication(u, sigma, v_transpose):
+    result = np.dot(u, np.dot(sigma, v_transpose))
+    return result
+
+u = np.array([
+    [np.sqrt(3)/2, 0, -1/2], 
+    [0, -1, 0],
+    [1/2, 0, np.sqrt(3)/2] 
+    ])
+
+sigma = np.array([
+    [3, 0],
+    [0, 1],
+    [0, 0]
+    ])
+
+v_transpose = np.array([
+    [np.sqrt(3)/2, -1/2],
+    [1/2, np.sqrt(3)/2]
+    ])
+
+X = matrix_multiplication(u, sigma, v_transpose)
+print(X)
