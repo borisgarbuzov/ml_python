dde4ac17c5339a0cc37138ffe21fe8f504be2ef6
statistics101 <boris.garbuzov@gmail.com>
2019-04-04 10:01:51 -0400

    svd

--- SVD/SVDExample.py
+++ SVD/SVDExample.py
@@ -23,19 +23,29 @@ v = np.array([
     [-1/2,          np.sqrt(3)/2]
     ])
 
+Identitty3 = np.identity(3)
+print("Identitty3 = \n", Identitty3)
+allOneThird = 1/3 * np.ones((3, 3))
+print("allOneThird = \n", allOneThird)
+centering3 = Identitty3 - allOneThird
+print("centering3 = \n", centering3)
+
 v_transpose = np.transpose(v)
 
 x = matrix_multiplication(u, sigma, v_transpose)
 print("x = ")
 print(x)
 
-c = np.dot(np.transpose(x), x)
-print("c = \n", c)
+xC = np.dot(centering3, x)
+print("xC = \n", xC)
 
-ker = np.dot(x, np.transpose(x))
-print("ker = \n", ker)
+xtx = np.dot(np.transpose(x), x)
+print("xtx = \n", xtx)
 
-cEigenvalues, cEigenvectors = np.linalg.eig(c)
+xxt = np.dot(x, np.transpose(x))
+print("xxt = \n", xxt)
+
+cEigenvalues, cEigenvectors = np.linalg.eig(xtx)
 print("cEigenvalues = \n", cEigenvalues)
 print("cEigenvectors = \n", cEigenvectors)
 print("v = \n", v)
@@ -54,6 +64,26 @@ print("x = \n", np.around(x,2))
 print("u = \n", np.around(u,2))
 
 
+
+
+xxtEigenvalues, xxtEigenvectors = np.linalg.eig(xxt)
+print("xxtEigenvalues = \n", xxtEigenvalues)
+print("xxtEigenvectors = \n", np.around(xxtEigenvectors,2))
+print("u = \n", np.around(u,2))
+
+uSigma = np.dot(u, sigma)
+print("uSigma = \n", np.around(uSigma,2))
+print("xPrime = \n", np.around(xPrime,2))
+
+
+# plt.show()
 plt.plot(x[:, 0], x[:, 1], 'o')
-plt.show()
+plt.plot(xPrime[:, 0], xPrime[:, 1], 'o')
+# plt.ylim(-1.4, 0)
 plt.savefig('plot_of_x.png')
+plt.show()
+plt.plot(xC[:, 0], xC[:, 1], 'o')
+
+# plt.ylim(-1.4, 0)
+plt.savefig('plot_of_xC.png')
+
