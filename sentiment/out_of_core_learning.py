7593cc8f58a612d9f8583c7d1c6e1c4b0dd03552
Maxim Makovskiy <makovskiyms@gmail.com>
2019-05-01 11:31:32 +0600

    sentiment analysis code for pycharm

---
+++ sentiment/out_of_core_learning.py
@@ -0,0 +1,57 @@
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.linear_model import SGDClassifier
+import numpy as np
+import re
+
+
+stop = stopwords.words('english')
+
+def tokenizer(text):
+    text = re.sub('<[^>]*>', '', text)
+    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text.lower())
+    text = re.sub('[\W]+', ' ', text.lower()) + ' '.join(emoticons).replace('-', '')
+    tokenized = [w for w in text.split() if w not in stop]
+    return tokenized
+
+def stream_docs(path):
+    with open(path, 'r', encoding='utf-8') as csv:
+        next(csv) # skip header
+        for line in csv:
+            text, label = line[:-3], int(line[-2])
+            yield text, label
+
+next(stream_docs(path='./movie_data.csv'))
+
+def get_minibatch(doc_stream, size):
+    docs, y = [], []
+    try:
+        for _ in range(size):
+            text, label = next(doc_stream)
+            docs.append(text)
+            y.append(label)
+    except StopIteration:
+        return None, None
+    return docs, y
+
+vect = HashingVectorizer(decode_error='ignore',
+                         n_features=2**21,
+                         preprocessor=None,
+                         tokenizer=tokenizer)
+
+clf = SGDClassifier(loss='log', random_state=1, max_iter=1, tol=None)
+doc_stream = stream_docs(path='./movie_data.csv')
+
+classes = np.array([0, 1])
+for _ in range(3):
+    X_train, y_train = get_minibatch(doc_stream, size=10)
+    if not X_train:
+        break
+    X_train = vect.transform(X_train)
+    clf.partial_fit(X_train, y_train, classes=classes)
+
+X_test, y_test = get_minibatch(doc_stream, size=10)
+X_test = vect.transform(X_test)
+print('Accuracy: %.3f' % clf.score(X_test, y_test))
+
+clf = clf.partial_fit(X_test, y_test)
