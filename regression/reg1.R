2ffdc95b78ca71f591504b489a688a2a1faeac7b
Boris Garbuzov <boris@ip-172-31-5-145.ca-central-1.compute.internal>
2019-05-16 14:00:47 +0000

    lesson

--- regression/reg1.R
+++ regression/reg1.R
@@ -1,7 +1,39 @@
 rm(list = ls())
-setwd("G:/mine/my_text/toronto_u/research/zhou/r/ml/ml_python/regression")
+# setwd("G:/mine/my_text/toronto_u/research/zhou/r/ml/ml_python/regression")
+setwd("~/ml_python/regression")
 df = read.csv("data1.csv")
 df
 xyLm = lm(df)
 xyLm
 plot(df)
+names(xyLm)
+myResiduals = residuals(xyLm)
+sum(myResiduals)
+coef(xyLm)
+effects(xyLm)
+#rank(xyLm)
+myFittedValues = fitted.values(xyLm)
+xyLm$xlevels
+
+
+# myXLevels = xlevels(xyLm)
+# myXLevels
+# rise = myFittedValues[2] - myFittedValues[1]
+# rise
+# run = myXLevels[2] - myXLevels[1]
+# run
+# slope = rise / run
+# slope
+# coefficients(xyLm)
+
+myX = c(1, 2, 3)
+xBar = mean(myX)
+xBar
+xCentered = myX - xBar
+xCentered
+sum(xCentered)
+
+
+
+
+
